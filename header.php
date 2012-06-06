b452d3a289e49c41e4bced573635d218bccbc0c8
Quasimo <linuximo@gmail.com>
2012-06-06 18:28:43 +0800

    some bugs fixed.

--- header.php
+++ header.php
@@ -10,12 +10,12 @@
 
 	<meta http-equiv="X-UA-Compatible" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />	
 	<meta name="description" content="<?php bloginfo('description'); ?>"><!-- quasimo plan excerpt -->
-	<?php while(have_posts()) : the_post(); ?><meta name="author" content="<?php the_author('display_name'); ?>"><?php endwhile; ?>
+
 	
 	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
 	
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/decolumn-h5bp.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 	
 	<!--[if lt IE 9]>
 	<script src="<?php bloginfo('template_directory'); ?>/js/libs/html5-3.4-respond-1.1.0.min.js"></script>
@@ -36,7 +36,7 @@
 	<div id="header-container">
 		<header class="wrapper clearfix">
 			<h1 id="title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo('name'); ?></a></h1>
-			<nav>
+			<nav class="clearfix">
 				<?php wp_nav_menu( array( 'theme_location' => 'topNav', 'container_class' => 'menu-top-nav-container' ) ); ?>
 			</nav>
 		</header>
